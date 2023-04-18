6c0b3a6056fdd2f6c8000c6c06d7b5d97ee18fc8
Yahia Eldakhakhny <yahia4360673@gmail.com>
2023-04-18 13:29:48 +0200

    fixed old_priority error

--- thread.h
+++ thread.h
@@ -87,9 +87,11 @@ struct thread
     enum thread_status status;          /* Thread state. */
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
-    int priority;                       /* Priority. */
+    int priority;
+    /**Modification*/                       /* Priority. */
+    int old_priority;                       /* Old Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
-
+	
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
